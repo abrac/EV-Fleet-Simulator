4709c4072f088c6b05971ea113022ae787fa7ecc
Chris Abraham <4852431-_abrac@users.noreply.gitlab.com>
2022-07-30 07:54:20 +0200

    Restore animation functionality.

--- src/data_processing_ev/data_visualisation/animation.py
+++ src/data_processing_ev/data_visualisation/animation.py
@@ -13,6 +13,7 @@ import data_processing_ev as dpr
 def _reformat_time(time_old, **kwargs):
     # time format: "year/month/day hr12:min:sec meridiem" -->
     # "year-month-day{T}hr24:min:sec"
+    time_old = str(time_old)
     input_data_fmt = kwargs.get('input_data_fmt')
     if input_data_fmt == dpr.DATA_FMTS['GPS']:
         (date, time) = time_old.split(' ')
@@ -52,11 +53,13 @@ def animate_route(df, map, **kwargs):
     # FIXME This is memory inefficient for large files.
     lines = []
     dfiterator = df.iterrows()
-    for (i, row) in dfiterator:
-        if i == 0:
+    first_iter = True
+    for _, row in dfiterator:
+        if first_iter:
             coord2_lat = row.Latitude
             coord2_lon = row.Longitude
             date2 = _reformat_time(row.Time, **kwargs)
+            first_iter = False
             continue
 
         coord1_lat = coord2_lat
@@ -122,21 +125,28 @@ def gen_scenario_animations(scenario_dir: Path,
     longitude = boundary.loc[:, 'Longitude'].mean()
     latitude = boundary.loc[:, 'Latitude'].mean()
     for trace_file in tqdm(traces_list):
+        trace_savedir = save_dir.joinpath(trace_file.stem)
+        trace_savedir.mkdir(parents=True, exist_ok=True)
+
         df = pd.read_csv(str(trace_file))
+        df['Time'] = pd.to_datetime(df['Time'])
 
-        # Initialise map around Stellenbosch
-        map_area = folium.Map(location=[latitude, longitude],
-                              titles=scenario_name, zoom_start=14)
+        for date, sub_df in df.groupby([df['Time'].dt.date]):
+            # Initialise map around Stellenbosch
+            map_area = folium.Map(location=[latitude, longitude],
+                                  titles=scenario_name, zoom_start=14)
 
-        animate_route(df, map_area, **kwargs)
+            animate_route(sub_df, map_area, **kwargs)
 
-        # save the map as an html
-        if saving:
-            save_file = save_dir.joinpath(
-                '{0}_{1}.html'.format(scenario_name, trace_file.stem))
-            map_area.save(str(save_file))
+            # save the map as an html
+            if saving:
+                save_file = trace_savedir.joinpath(
+                    '{0}_{1}_{2}.html'.format(scenario_name,
+                                              trace_file.stem,
+                                              str(date)))
+                map_area.save(str(save_file))
 
-        yield map_area
+            yield map_area
 
 
 def animate_scenario(scenario_dir: Path, saving: bool = True,
