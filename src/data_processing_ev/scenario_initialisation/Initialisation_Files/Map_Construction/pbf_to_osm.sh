ded3252603407282548492216e210e5931dabc32
Chris Abraham <4852431-_abrac@users.noreply.gitlab.com>
2022-09-12 02:14:34 +0000

    Fix bbox switch in pbf-to-osm.sh script

--- src/data_processing_ev/scenario_initialisation/Initialisation_Files/Map_Construction/pbf_to_osm.sh
+++ src/data_processing_ev/scenario_initialisation/Initialisation_Files/Map_Construction/pbf_to_osm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-osmium extract --strategy complete_ways -bbox <min_lon>,<min_lat>,<max_lon>,<max_lat> *.osm.pbf -o extract.osm.pbf
+osmium extract --strategy complete_ways --bbox <min_lon>,<min_lat>,<max_lon>,<max_lat> *.osm.pbf -o extract.osm.pbf
 osmium cat extract.osm.pbf -o square_boundary.osm
