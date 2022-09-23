7e9e6b10387d401554c02f43841ee56e2a8aafd2
Chris Abraham <4852431-_abrac@users.noreply.gitlab.com>
2022-09-23 20:04:53 +0200

    Make pbf_to_osm script remove temp file.

--- src/data_processing_ev/scenario_initialisation/Initialisation_Files/Map_Construction/pbf_to_osm.sh
+++ src/data_processing_ev/scenario_initialisation/Initialisation_Files/Map_Construction/pbf_to_osm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-osmium extract --strategy complete_ways --bbox <min_lon>,<min_lat>,<max_lon>,<max_lat> *.osm.pbf -o extract.osm.pbf
-osmium cat extract.osm.pbf -o square_boundary.osm
+osmium extract --strategy complete_ways --bbox <min_lon>,<min_lat>,<max_lon>,<max_lat> *.osm.pbf -o temp.osm.pbf
+osmium cat temp.osm.pbf -o square_boundary.osm
+rm temp.osm.pbf
