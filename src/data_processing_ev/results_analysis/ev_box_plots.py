ae878e3bcc0f29c005e650fb2d7d5c1628aa701e
Chris Abraham <4852431-_abrac@users.noreply.gitlab.com>
2022-11-17 19:31:05 +0200

    Generate flattened and non-flattened energy box plots together

--- src/data_processing_ev/results_analysis/ev_box_plots.py
+++ src/data_processing_ev/results_analysis/ev_box_plots.py
@@ -9,6 +9,7 @@ executed from the within the scenario's simulation-outputs directory.
 from pathlib import Path
 import json
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 import pandas as pd
 from itertools import repeat
@@ -16,23 +17,18 @@ import pickle
 import data_processing_ev as dpr
 
 
-def plot_ev_energy_boxes(scenario_dir: Path, plot_blotches: bool = False,
-                         figsize=(4, 3), **kwargs):
-
-    _ = input("Would you like to plot the box-plots of the fleet's energy " +
-              "usage? [y]/n  ")
-    if _.lower() == 'n':
-        return
-
-    input_data_fmt = kwargs.get('input_data_fmt', dpr.DATA_FMTS['GPS'])
+def _gen_box_plots(scenario_dir: Path,
+                  flatten_mode: bool = False,
+                  input_data_fmt=dpr.DATA_FMTS['GPS'],
+                  figsize=(4, 3),
+                  plot_blotches: bool = False,
+                  **kwargs) -> plt.Figure:
 
     # Get a list of paths to the stats.json files of each taxi.
     simulation_outputs_dir = scenario_dir.joinpath('EV_Results')
     ev_stats_files = sorted([
         *simulation_outputs_dir.glob('*/Outputs/stats_*.json')
     ])
-
-    box_plots_dir = simulation_outputs_dir.joinpath('Outputs', 'Box_Plots')
 
     # for each ev, read statistics and generate a box plot
     all_date_strs = []
@@ -70,12 +66,11 @@ def plot_ev_energy_boxes(scenario_dir: Path, plot_blotches: bool = False,
         ev_csv_files = sorted([
             *ev_dir.glob('*/battery.out.csv')])
         for ev_csv_file in ev_csv_files:
-            ev_df = pd.read_csv(ev_csv_file)
+            ev_df = pd.read_csv(ev_csv_file, sep="\\s*,\\s*", engine='python')
             dist_travelled = ev_df['vehicle_speed'].sum()
             dists_travelled.append(dist_travelled)
         all_dists_travelled.append(dists_travelled)
 
-    flatten_mode = False  # TODO Make this a configurable parameter.
     if input_data_fmt == dpr.DATA_FMTS['GTFS'] or flatten_mode:
         # Flatten the energy diffs array, so that only one box plot is created.
         all_energy_diffs_grouped = all_energy_diffs
@@ -106,23 +101,7 @@ def plot_ev_energy_boxes(scenario_dir: Path, plot_blotches: bool = False,
                fontsize='small')
     plt.xlabel("eMBT ID")
     plt.tight_layout()
-
-    # Output box-plots.
-    box_plots_dir.mkdir(parents=True, exist_ok=True)
-    # As png:
-    fig_dir = box_plots_dir.joinpath("Energy_usage_box_plots.png")
-    plt.savefig(fig_dir)
-    # As svg:
-    fig_dir = box_plots_dir.joinpath("Energy_usage_box_plots.pdf")
-    plt.savefig(fig_dir)
-    # As pickle:
-    fig_dir = box_plots_dir.joinpath(
-        "Energy_usage_box_plots.fig.pickle")
     fig = plt.gcf()
-    pickle.dump(fig, open(fig_dir, 'wb'))
-
-    # Convert all_energy_diffs to a dataframe and save as a csv file.
-    csv_dir = box_plots_dir.joinpath("Energy_usage.csv")
 
     if input_data_fmt == dpr.DATA_FMTS['GTFS'] or flatten_mode:
         # Restore (or unflatten) the energy diffs array.
@@ -142,6 +121,44 @@ def plot_ev_energy_boxes(scenario_dir: Path, plot_blotches: bool = False,
         columns=['ev_name', 'date', 'energy_used', 'dist_travelled'])
     df.set_index(['ev_name', 'date'])  # This is not necessary for the saving
         # process, but it's useful in-case this script is extended.
-    df.to_csv(csv_dir, index=False)
+
+    return fig, df
+
+
+def plot_ev_energy_boxes(scenario_dir: Path, **kwargs):
+
+    _ = input("Would you like to plot the box-plots of the fleet's energy " +
+              "usage? [y]/n  ")
+    if _.lower() == 'n':
+        return
+
+    # _ = input("Would youl like to flatten the box-plots? y/[n]  ")
+    # flatten_mode = False if _.lower() != 'y' else True
+
+    input_data_fmt = kwargs.get('input_data_fmt', dpr.DATA_FMTS['GPS'])
+    box_plots_dir = scenario_dir.joinpath('EV_Results', 'Outputs', 'Box_Plots')
+
+    for flatten_mode in (False, True):
+
+        dir_name = 'flattened' if flatten_mode else 'non_flattened'
+        box_plots_subdir = box_plots_dir.joinpath(dir_name)
+        box_plots_subdir.mkdir(parents=True, exist_ok=True)
+
+        fig, df = _gen_box_plots(scenario_dir, flatten_mode, input_data_fmt)
+
+        # As png:
+        fig_dir = box_plots_subdir.joinpath("Energy_usage_box_plots.png")
+        fig.savefig(fig_dir)
+        # As svg:
+        fig_dir = box_plots_subdir.joinpath("Energy_usage_box_plots.pdf")
+        fig.savefig(fig_dir)
+        # As pickle:
+        fig_dir = box_plots_subdir.joinpath(
+            "Energy_usage_box_plots.fig.pickle")
+        pickle.dump(fig, open(fig_dir, 'wb'))
+
+        # Convert all_energy_diffs to a dataframe and save as a csv file.
+        csv_dir = box_plots_subdir.joinpath("Energy_usage.csv")
+        df.to_csv(csv_dir, index=False)
 
     plt.show()
